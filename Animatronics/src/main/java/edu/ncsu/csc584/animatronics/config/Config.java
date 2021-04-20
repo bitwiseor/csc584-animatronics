cbbe0474a4491d5d3b456c3c949d229b39cf78ff
bitwiseor <43662677+bitwiseor@users.noreply.github.com>
2021-04-19 21:25:00 -0400

    config file from tutorial

---
+++ Animatronics/src/main/java/edu/ncsu/csc584/animatronics/config/Config.java
@@ -0,0 +1,36 @@
+package edu.ncsu.csc584.animatronics.config;
+
+import java.io.File;
+
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
+
+import edu.ncsu.csc584.animatronics.AnimatronicsMod;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class Config 
+{
+    private static final ForgeConfigSpec.Builder SERVER_BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SERVER;
+    
+    private static final ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec CLIENT;
+    
+    static
+    {
+        SERVER = SERVER_BUILDER.build();
+        CLIENT = CLIENT_BUILDER.build();
+    }
+    
+    public static void loadConfig(ForgeConfigSpec config, String path)
+    {
+        AnimatronicsMod.LOGGER.info("Loading config: " + path);
+        final CommentedFileConfig file = CommentedFileConfig.builder(new File(path)).sync().autosave().writingMode(WritingMode.REPLACE).build();
+        AnimatronicsMod.LOGGER.info("Built config: " + path);
+        file.load();
+        AnimatronicsMod.LOGGER.info("Loaded config: " + path);
+        config.setConfig(file);
+    }
+}
